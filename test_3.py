4457ba810605c5cb48d0a6bbde0a89fbf6001e44
Lustres <logansong@icloud.com>
2017-03-01 21:51:44 +0800

    :white_check_mark: Add test on DPDA

---
+++ test_3.py
@@ -0,0 +1,13 @@
+def test_DPDA():
+    import DPDA
+    rulebook = DPDA.DPDARuleBook([
+        DPDA.PDARule(1, '(', 2, '$', ['b', '$']),
+        DPDA.PDARule(2, '(', 2, 'b', ['b', 'b']),
+        DPDA.PDARule(2, ')', 2, 'b', []),
+        DPDA.PDARule(2, None, 1, '$', ['$'])
+    ])
+    dpda_design = DPDA.DFDADesign(1, [1], rulebook)
+    assert dpda_design.is_accepted('(((((((((())))))))))') == True
+    assert dpda_design.is_accepted('()(())((()))(()(())))') == False
+    assert dpda_design.is_accepted('(()(()(()()(()()))()') == False
+    assert dpda_design.is_accepted('())') == False
