d60c399bf7d31d04225490a1b367e04f4964de4b
Lustres <logansong@icloud.com>
2017-03-02 10:27:00 +0800

    :pencil2: Rename "DFDADesign" to DPDADesign

--- test_3.py
+++ test_3.py
@@ -6,7 +6,7 @@ def test_DPDA():
         DPDA.PDARule(2, ')', 2, 'b', []),
         DPDA.PDARule(2, None, 1, '$', ['$'])
     ])
-    dpda_design = DPDA.DFDADesign(1, [1], rulebook)
+    dpda_design = DPDA.DPDADesign(1, [1], rulebook)
     assert dpda_design.is_accepted('(((((((((())))))))))') == True
     assert dpda_design.is_accepted('()(())((()))(()(())))') == False
     assert dpda_design.is_accepted('(()(()(()()(()()))()') == False
