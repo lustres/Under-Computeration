68071cba9ad7700bd687cee6b609aa9bc1469e5e
Lustres <logansong@icloud.com>
2017-03-02 11:07:55 +0800

    :white_check_mark: Adding test

--- test_3.py
+++ test_3.py
@@ -11,3 +11,19 @@ def test_DPDA():
     assert dpda_design.is_accepted('()(())((()))(()(())))') == False
     assert dpda_design.is_accepted('(()(()(()()(()()))()') == False
     assert dpda_design.is_accepted('())') == False
+
+def test_DPDA_2():
+    import DPDA
+    rulebook = DPDA.DPDARuleBook([
+        DPDA.PDARule(1, 'a', 2, '$', ['a', '$']),
+        DPDA.PDARule(1, 'b', 2, '$', ['b', '$']),
+        DPDA.PDARule(2, 'a', 2, 'a', ['a', 'a']),
+        DPDA.PDARule(2, 'b', 2, 'b', ['b', 'b']),
+        DPDA.PDARule(2, 'a', 2, 'b', []),
+        DPDA.PDARule(2, 'b', 2, 'a', []),
+        DPDA.PDARule(2, None, 1, '$', ['$'])
+    ])
+    dpda_design = DPDA.DPDADesign(1, [1], rulebook)
+    assert dpda_design.is_accepted('ababab') == True
+    assert dpda_design.is_accepted('bbbaaaab') == True
+    assert dpda_design.is_accepted('baa') == False
