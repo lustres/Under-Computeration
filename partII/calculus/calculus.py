a5fa5c75da3dbebbaab9de8625bf0564908addbe
Lustres <logansong@icloud.com>
2017-03-17 16:10:31 +0800

    :sparkles: Add reduce method to simulate lambda semantic

--- partII/calculus/calculus.py
+++ partII/calculus/calculus.py
@@ -1,6 +1,6 @@
 class Variable:
     """
-    Variable objects could be captured or passed in lambda expression
+    Variable objects could be captured or passed in lambda expression.
     """
     def __init__(self, name):
         """
@@ -8,6 +8,19 @@ class Variable:
         :param name: a string used to refer this object
         """
         self.name = name
+
+    def replace(self, name, another):
+        """
+        replace a variable with another expression.
+        :type str
+        :param name: name of aim variable
+        :param another: an expression used to replace
+        :return: expression passed in if name marched or self
+        """
+        if self.name == name:
+            return another
+        else:
+            return self
 
     def __repr__(self):
         return self.name
@@ -28,6 +41,41 @@ class Function:
         self.parameter = parameter
         self.body = body
 
+    def replace(self, name, another):
+        """
+        replace a free variable in the body of this function with another expression.
+        Please make sure replacement doesn't have free variable of which name appear in aim function.
+
+        The free variable in replacement maybe captured by outer lambda parameter in accident .
+            eg:
+                e = lambda x: x(y)
+                let y = z(x)
+                => e = lambda x: x(z(x))
+                                     ^ free variable captured
+
+        The right behavior is rename parameter when conflict happened:
+            eg:
+                e = lambda x: x(y)
+                let y = z(x) # free variable conflict
+                => e = lambda w: w(y) # rename parameter with a unique id
+                     = lambda w: w(z(x))
+
+        We could record variable name used in term to solve this problem.
+        This implement are still effective as long as we make sure the replacement never contain free variable.
+        :type str
+        :param name: name of aim variable
+        :param another: an expression used to replace
+        :type Function
+        :return: function after replace in if name marched or self
+        """
+        if self.parameter == name:
+            return self
+        else:
+            return Function(self.parameter, self.body.replace(name, another))
+
+    def call(self, argument):
+        return self.body.replace(self.parameter, argument)
+
     def __repr__(self):
         return f"lambda {self.parameter}: {self.body}"
 
@@ -46,5 +94,45 @@ class Call:
         self.left = left
         self.right = right
 
+    def replace(self, name, another):
+        """
+        replace a variable in this call with another expression.
+        :type str
+        :param name: name of aim variable
+        :param another: an expression used to replace
+        :type Call
+        :return: Call after replace in if name marched or self
+        """
+        return Call(self.left.replace(name, another), self.right.replace(name, another))
+
+    def reduce(self):
+        """
+        reduce a call expression
+        This method will call `reduce` on two parts recursively.
+        And then make call on it self.
+        """
+        if is_reducible(self.left):
+            return Call(self.left.reduce(), self.right)
+        elif is_reducible(self.right):
+            return Call(self.left, self.right.reduce())
+        else:
+            return self.left.call(self.right)
+
     def __repr__(self):
         return f"{self.left}({self.right})"
+
+
+def is_reducible(term):
+    """
+    check a term whether reducible, raise a TypeError when term is not Variable, Function or Call
+    :type Variable | Function | Call
+    :param term: must be a instance of Variable, Function or Call
+    :type bool
+    :return: True if term is reducible or False
+    """
+    if isinstance(term, Variable) or isinstance(term, Function):
+        return False
+    elif isinstance(term, Call):
+        return is_reducible(term.left) or is_reducible(term.right) or isinstance(term.left, Function)
+    else:
+        raise TypeError()
--- partII/calculus/test_calculus.py
+++ partII/calculus/test_calculus.py
@@ -1,28 +1,82 @@
 from calculus import *
 
 
+one = Function('p', Function('x', Call(Variable('p'), Variable('x'))))
+
+increment = Function('n', Function('p', Function('x',
+                                                 Call(
+                                                     Variable('p'),
+                                                     Call(
+                                                         Call(
+                                                             Variable('n'), Variable('p')),
+                                                         Variable('x'))))))
+
+add = Function('m', Function('n', Function('f', Function('x',
+                                                         Call(
+                                                             Call(
+                                                                 Variable('m'), Variable('f')),
+                                                             Call(
+                                                                 Call(
+                                                                     Variable('n'), Variable('f')),
+                                                                 Variable('x')))))))
 def test_grammar():
-    one = Function('p', Function('x', Call(Variable('p'), Variable('x'))))
     assert repr(one) == 'lambda p: lambda x: p(x)'
 
 
 def test_grammar_2():
-    increment = Function('n', Function('p', Function('x',
-                                                     Call(
-                                                         Variable('p'),
-                                                         Call(
-                                                             Call(
-                                                                 Variable('n'), Variable('p')),
-                                                             Variable('x'))))))
     assert repr(increment) == 'lambda n: lambda p: lambda x: p(n(p)(x))'
 
 def test_grammar_3():
-    add = Function('m', Function('n', Function('f', Function('x',
-                                                             Call(
-                                                                 Call(
-                                                                     Variable('m'), Variable('f')),
-                                                                 Call(
-                                                                     Call(
-                                                                         Variable('n'), Variable('f')),
-                                                                     Variable('x')))))))
     assert repr(add) == 'lambda m: lambda n: lambda f: lambda x: m(f)(n(f)(x))'
+
+
+def test_semantic_replace():
+    e = Variable('x')
+    assert repr(e.replace('x', Function('y', Variable('y')))) == 'lambda y: y'
+    assert repr(e.replace('z', Function('y', Variable('y')))) == repr(e)
+
+
+def test_semantic_replace_2():
+    e = Call(
+            Call(
+                Call(Variable('a'), Variable('b')),
+                Variable('c')),
+            Variable('b'))
+    assert repr(e.replace('a', Variable('x'))) == 'x(b)(c)(b)'
+    assert repr(e.replace('b', Function('x', Variable('x')))) == 'a(lambda x: x)(c)(lambda x: x)'
+
+
+def test_semantic_replace_3():
+    e = Call(
+            Call(
+                Variable('x'), Variable('y')),
+            Function('y',
+                     Call(Variable('y'), Variable('x'))))
+    assert repr(e.replace('x', Variable('z'))) == 'z(y)(lambda y: y(z))'
+    assert repr(e.replace('y', Variable('z'))) == 'x(z)(lambda y: y(x))'
+
+
+def test_semantic_replace_4():
+    """
+    This function shows the bug in replace method.
+    """
+    e = Function('x',
+                 Call(Variable('x'), Variable('y')))
+    replacement = Call(Variable('z'), Variable('x'))
+    assert repr(e.replace('y', replacement)) == 'lambda x: x(z(x))'
+
+
+def test_semantic_call():
+    f = Function('x', Function('y', Call(Variable('x'), Variable('y'))))
+    arg = Function('z', Variable('z'))
+    assert repr(f.call(arg)) == 'lambda y: lambda z: z(y)'
+
+
+def test_semantic_reduce():
+    e = Call(Call(add, one), one)
+    inc = Variable('inc')
+    zero = Variable('zero')
+    e = Call(Call(e, inc), zero)
+    while is_reducible(e):
+        e = e.reduce()
+    assert repr(e) == 'inc(inc(zero))'
