07f482fde63a1d2b9f729f0a841f3bab513f0d79
lustres <Lustres@users.noreply.github.com>
2017-04-22 16:24:29 +0800

    Completeness (#10)
    Give a stronger implement of reduce

--- partII/calculus/calculus.py
+++ partII/calculus/calculus.py
@@ -1,4 +1,9 @@
-class Variable:
+class Term(object):
+    def __init__(self):
+        super(Term, self).__init__()
+
+
+class Variable(Term):
     """
     Variable objects could be captured or passed in lambda expression.
     """
@@ -7,6 +12,7 @@ class Variable:
         :type str
         :param name: a string used to refer this object
         """
+        super(Variable, self).__init__()
         self.name = name
 
     def replace(self, name, another):
@@ -15,6 +21,7 @@ class Variable:
         :type str
         :param name: name of aim variable
         :param another: an expression used to replace
+        :type Variable
         :return: expression passed in if name marched or self
         """
         if self.name == name:
@@ -22,11 +29,29 @@ class Variable:
         else:
             return self
 
-    def __repr__(self):
+    def bound_vars(self):
+        """
+        Variable never bound variable.
+        """
+        return {}
+
+    def free_vars(self):
+        """
+        Variable always introduce free variable into context.
+        """
+        return {self.name}
+
+    def reduce(self):
+        return self
+
+    def __str__(self):
         return self.name
 
+    def __repr__(self):
+        return f"Variable({repr(self.name)})"
 
-class Function:
+
+class Function(Term):
     """
     Function objects represent a single parameter lambda expression.
     """
@@ -38,49 +63,113 @@ class Function:
         :type Function | Call | Variable
         :param body: the rest of lambda
         """
+        super(Function, self).__init__()
         self.parameter = parameter
         self.body = body
 
     def replace(self, name, another):
         """
         replace a free variable in the body of this function with another expression.
-        Please make sure replacement doesn't have free variable of which name appear in aim function.
-
-        The free variable in replacement maybe captured by outer lambda parameter in accident .
-            eg:
-                e = lambda x: x(y)
-                let y = z(x)
-                => e = lambda x: x(z(x))
-                                     ^ free variable captured
-
-        The right behavior is rename parameter when conflict happened:
-            eg:
-                e = lambda x: x(y)
-                let y = z(x) # free variable conflict
-                => e = lambda w: w(y) # rename parameter with a unique id
-                     = lambda w: w(z(x))
-
-        We could record variable name used in term to solve this problem.
-        This implement are still effective as long as we make sure the replacement never contain free variable.
+        if body doesn't have matched free variable, the method return self
+        if you want replace a bound variable, use `alpha` instead
         :type str
         :param name: name of aim variable
         :param another: an expression used to replace
         :type Function
         :return: function after replace in if name marched or self
         """
-        if self.parameter == name:
+        if name not in self.free_vars():
+            # don't have aim free var
             return self
+        elif self.bound_vars() & another.free_vars():
+            # capture conflict
+            # use alpha conversion to rename bound var aka. parameter
+            return alpha(self, rename_policy(self.parameter)).replace(name, another)
         else:
+            # have aim free var
+            # no conflict
             return Function(self.parameter, self.body.replace(name, another))
 
-    def call(self, argument):
-        return self.body.replace(self.parameter, argument)
+    def bound_vars(self):
+        """
+        Function only bound variable once as its parameter.
+        """
+        return {self.parameter}
 
-    def __repr__(self):
+    def free_vars(self):
+        """
+        Function will bound its parameter on body.
+        """
+        return self.body.free_vars() - self.bound_vars()
+
+    def reduce(self):
+        eta_ans = eta(self)
+        if isinstance(eta_ans, Function):
+            return Function(eta_ans.parameter, eta_ans.body.reduce())
+        else:
+            return eta_ans
+
+    def __str__(self):
         return f"lambda {self.parameter}: {self.body}"
 
+    def __repr__(self):
+        return f"Function({repr(self.parameter)}, {repr(self.body)})"
 
-class Call:
+def alpha(func, rename, aim = None):
+    """
+    :type Function
+    :param func:
+    :type Variable
+    :param rename:
+    """
+    if isinstance(func, Function):
+        if aim is None:
+            # first call
+            # replace parameter and apply `alpha` to body
+            return Function(rename, alpha(func.body, rename, func.parameter))
+        else:
+            # not first call
+            if func.parameter is aim:
+                # inner bounded
+                return func
+            else:
+                # apply `alpha` to body
+                return Function(func.parameter, alpha(func.body, rename, aim))
+    elif isinstance(func, Call):
+        return Call(alpha(func.left, rename, aim), alpha(func.right, rename, aim))
+    elif isinstance(func, Variable):
+        if func.name == aim:
+            return rename
+        else:
+            return func
+
+
+def beta(func, argument):
+    """
+    :type Function
+    :param func:
+    """
+    return func.body.replace(func.parameter, argument)
+
+
+def eta(func):
+    """
+    :type Function
+    :param func:
+    """
+    if isinstance(func, Function) and isinstance(func.body, Call) \
+            and str(func.body.right) is func.parameter \
+            and func.parameter not in func.body.left.free_vars():
+        return eta(func.body.left)
+    else:
+        return func
+
+
+def rename_policy(name):
+    return Variable(name + "'")
+
+
+class Call(Term):
     """
     Call objects define a call action on an entity.
     """
@@ -91,6 +180,7 @@ class Call:
         :type Function | Call | Variable
         :param right: the parameter which will be passed in
         """
+        super(Call, self).__init__()
         self.left = left
         self.right = right
 
@@ -111,15 +201,38 @@ class Call:
         This method will call `reduce` on two parts recursively.
         And then make call on it self.
         """
-        if is_reducible(self.left):
-            return Call(self.left.reduce(), self.right)
-        elif is_reducible(self.right):
+        # reduce argument first
+        if is_reducible(self.right):
             return Call(self.left, self.right.reduce())
+        elif is_reducible(self.left):
+            return Call(self.left.reduce(), self.right)
+        elif isinstance(self.left, Function):
+            # reduce all part so make call
+            return beta(self.left, self.right)
         else:
-            return self.left.call(self.right)
+            # reduce all part and finish
+            return self
+
+    def bound_vars(self):
+        """
+        Call never bound variable cause all bounded value inside are not visible outside.
+        """
+        return {}
+
+    def free_vars(self):
+        """
+        All free variable in sub pair of Call are still visible outside.
+        """
+        return self.left.free_vars() | self.right.free_vars()
+
+    def __str__(self):
+        if isinstance(self.left, Function):
+            return f"({self.left})({self.right})"
+        else:
+            return f"{self.left}({self.right})"
 
     def __repr__(self):
-        return f"{self.left}({self.right})"
+        return f"Call({repr(self.left)}, {repr(self.right)})"
 
 
 def is_reducible(term):
@@ -134,5 +247,16 @@ def is_reducible(term):
         return False
     elif isinstance(term, Call):
         return is_reducible(term.left) or is_reducible(term.right) or isinstance(term.left, Function)
-    else:
-        raise TypeError()
+
+
+def reduce(term):
+    while is_reducible(term):
+        term = term.reduce()
+
+    while True:
+        new_term = term.reduce()
+        if str(new_term) == str(term):
+            break
+        term = new_term
+
+    return term
--- partII/calculus/test_calculus.py
+++ partII/calculus/test_calculus.py
@@ -20,20 +20,26 @@ add = Function('m', Function('n', Function('f', Function('x',
                                                                      Variable('n'), Variable('f')),
                                                                  Variable('x')))))))
 def test_grammar():
-    assert repr(one) == 'lambda p: lambda x: p(x)'
+    assert str(one) == 'lambda p: lambda x: p(x)'
 
 
 def test_grammar_2():
-    assert repr(increment) == 'lambda n: lambda p: lambda x: p(n(p)(x))'
+    assert str(increment) == 'lambda n: lambda p: lambda x: p(n(p)(x))'
+
 
 def test_grammar_3():
-    assert repr(add) == 'lambda m: lambda n: lambda f: lambda x: m(f)(n(f)(x))'
+    assert str(add) == 'lambda m: lambda n: lambda f: lambda x: m(f)(n(f)(x))'
+
+
+def test_grammar_4():
+    e = Function('x', Function('y', Call(Function('z', Call('z', 'x')), Variable('y'))))
+    assert str(e) == "lambda x: lambda y: (lambda z: z(x))(y)"
 
 
 def test_semantic_replace():
     e = Variable('x')
-    assert repr(e.replace('x', Function('y', Variable('y')))) == 'lambda y: y'
-    assert repr(e.replace('z', Function('y', Variable('y')))) == repr(e)
+    assert str(e.replace('x', Function('y', Variable('y')))) == 'lambda y: y'
+    assert str(e.replace('z', Function('y', Variable('y')))) == str(e)
 
 
 def test_semantic_replace_2():
@@ -42,8 +48,8 @@ def test_semantic_replace_2():
                 Call(Variable('a'), Variable('b')),
                 Variable('c')),
             Variable('b'))
-    assert repr(e.replace('a', Variable('x'))) == 'x(b)(c)(b)'
-    assert repr(e.replace('b', Function('x', Variable('x')))) == 'a(lambda x: x)(c)(lambda x: x)'
+    assert str(e.replace('a', Variable('x'))) == 'x(b)(c)(b)'
+    assert str(e.replace('b', Function('x', Variable('x')))) == 'a(lambda x: x)(c)(lambda x: x)'
 
 
 def test_semantic_replace_3():
@@ -52,31 +58,68 @@ def test_semantic_replace_3():
                 Variable('x'), Variable('y')),
             Function('y',
                      Call(Variable('y'), Variable('x'))))
-    assert repr(e.replace('x', Variable('z'))) == 'z(y)(lambda y: y(z))'
-    assert repr(e.replace('y', Variable('z'))) == 'x(z)(lambda y: y(x))'
+    assert str(e.replace('x', Variable('z'))) == 'z(y)(lambda y: y(z))'
+    assert str(e.replace('y', Variable('z'))) == 'x(z)(lambda y: y(x))'
 
 
 def test_semantic_replace_4():
-    """
-    This function shows the bug in replace method.
-    """
     e = Function('x',
                  Call(Variable('x'), Variable('y')))
     replacement = Call(Variable('z'), Variable('x'))
-    assert repr(e.replace('y', replacement)) == 'lambda x: x(z(x))'
+    assert str(e.replace('y', replacement)) == "lambda x': x'(z(x))"
 
 
-def test_semantic_call():
-    f = Function('x', Function('y', Call(Variable('x'), Variable('y'))))
-    arg = Function('z', Variable('z'))
-    assert repr(f.call(arg)) == 'lambda y: lambda z: z(y)'
+def test_semantic_replace_5():
+    # e = lambda p: lambda x: lambda z: (lambda x: y(p(x)))(x)(z)
+    e = Function('p', Function('x', Function('z', Call(Call(Function('x', Call(Variable('y'), Call(Variable('p'), Variable('x')))), Variable('x')), Variable('z')))))
+    replacement = Call(Variable('z'), Variable('x'))
+    assert str(e.replace('y', replacement)) == "lambda p: lambda x': lambda z': (lambda x': z(x)(p(x')))(x')(z')"
 
 
 def test_semantic_reduce():
     e = Call(Call(add, one), one)
-    inc = Variable('inc')
-    zero = Variable('zero')
-    e = Call(Call(e, inc), zero)
-    while is_reducible(e):
-        e = e.reduce()
-    assert repr(e) == 'inc(inc(zero))'
+    assert str(reduce(e)) == 'lambda f: lambda x: f(f(x))'
+
+
+def test_semantic_reduce_2():
+    e = Function('x', Call(Function('p', Function('x', Variable('x'))), Variable('x')))
+    assert str(reduce(e)) == "lambda p: lambda x: x"
+
+
+def test_alpha():
+    e = alpha(one, Variable('f'))
+    assert str(e) == 'lambda f: lambda x: f(x)'
+
+    e = alpha(increment, Variable('g'))
+    assert str(e) == 'lambda g: lambda p: lambda x: p(g(p)(x))'
+
+    e = alpha(add, Variable('q'))
+    assert str(e) == 'lambda q: lambda n: lambda f: lambda x: q(f)(n(f)(x))'
+
+
+def test_alpha_2():
+    e = Function('x', Function('y', Call(Function('x', Call(Variable('x'), Variable('y'))), Variable('x'))))
+    assert str(e) == 'lambda x: lambda y: (lambda x: x(y))(x)'
+    e = alpha(e, Variable('w'))
+    assert str(e) == 'lambda w: lambda y: (lambda x: x(y))(w)'
+
+
+def test_beta():
+    f = Function('x', Function('y', Call(Variable('x'), Variable('y'))))
+    arg = Function('z', Variable('z'))
+    assert str(beta(f, arg)) == 'lambda y: (lambda z: z)(y)'
+
+
+def test_eta():
+    e = Function('x', Call(Variable('f'), Variable('x')))
+    assert str(eta(e)) == 'f'
+
+
+def test_eta_2():
+    e = lambda x: lambda y: lambda z: x(y)(z)(x)
+    e = Function('x', Function('y', Function('z', Call(Call(Call(Variable('x'), Variable('y')),Variable('z')),Variable('x')))))
+    assert str(eta(e)) == 'lambda x: lambda y: lambda z: x(y)(z)(x)'
+
+
+def test_repr():
+    assert repr(one) == "Function('p', Function('x', Call(Variable('p'), Variable('x'))))"
